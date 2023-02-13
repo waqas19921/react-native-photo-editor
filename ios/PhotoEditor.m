24cd4b9a7c21a1d3c5a26ffbcd51ba7ea2f30ca3
waqas19921 <waqas19921@gmail.com>
2023-02-13 11:53:08 +0500

    iOS warning fixed. version updates.

--- ios/PhotoEditor.m
+++ ios/PhotoEditor.m
@@ -2,6 +2,8 @@
 
 @interface RCT_EXTERN_MODULE(PhotoEditor, NSObject)
 
++(BOOL)requiresMainQueueSetup  {  return NO;  };
+
 RCT_EXTERN_METHOD(open:(NSDictionary *)options
                  withResolver:(RCTPromiseResolveBlock)resolve
                  withRejecter:(RCTPromiseRejectBlock)reject)
